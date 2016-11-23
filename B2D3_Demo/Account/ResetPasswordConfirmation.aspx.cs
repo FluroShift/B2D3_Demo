8b14d5fa1732a0aecf71d1be2037000b36f62300
Dwayne <Dwayne@DWAYNE-LAPTOP>
2016-11-23 11:16:56 +0100

    Add project files.

---
+++ B2D3_Demo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace B2D3_Demo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ B2D3_Demo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(B2D3_Demo.Startup))]
+namespace B2D3_Demo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
